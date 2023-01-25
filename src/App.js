d301fc6a4450c0b5b99198283e4e92da14540a4f
karl wernsman <wernsmank@gmail.com>
2023-01-25 15:20:07 -0800

    removed to from Player in App.js

--- src/App.js
+++ src/App.js
@@ -52,9 +52,9 @@ function App() {
     <div className="App">
       <section>
         {/* if the player names are numbers, that will make our life easier later because we can reuse numbers as arrays. Note that this will make our app brittle! */}
-        <Player to={to} player={1} hand={playerOneHand} />
-        <Player to={to} player={2} hand={playerTwoHand} />
-        <Player to={to} player={3} hand={playerThreeHand} />
+        <Player player={1} hand={playerOneHand} />
+        <Player player={2} hand={playerTwoHand} />
+        <Player player={3} hand={playerThreeHand} />
         <CardList cards={deck} player={'deck'} />
       </section>
       <section>{selectedCard && <ExecutePassButton passCard={passCard} cards={deck} />}</section>
